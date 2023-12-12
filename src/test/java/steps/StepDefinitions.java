6fdeb712687bd397a5005d663cf554216b74fca4
Nickolay Petrych <npetrych@alfabank.ru>
2023-12-12 19:40:55 +0000

    add scenario

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -4,16 +4,29 @@ import io.cucumber.java.en.*;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("confirm cookie in start google page")
+    public void confirm_cookie_in_start_google_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @When("enter a search word {string}")
+    public void enter_a_search_word(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @And("find an article on the official website of Cucumber")
+    public void found_off_website_of_cucumber() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+    @Then("ensure that topic of quick start is onsite")
+    public void ensure_that_topic_of_quick_start_is_onsite() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 }
---
+++ src/test/resources/scenarios/google_search.feature
@@ -0,0 +1,9 @@
+Feature: Searching in google article about Cucumber tool
+
+  Background: the google search is open
+
+  Scenario: searching in google article
+    Given confirm cookie in start google page
+    When enter a search word "Cucumber"
+    And find an article on the official website of Cucumber
+    Then ensure that topic of quick start is onsite
