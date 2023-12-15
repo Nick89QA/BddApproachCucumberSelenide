d712d0ed0aa419a446ac908d532009d4979f7717
Nickolay Petrych <npetrych@alfabank.ru>
2023-12-15 13:38:32 +0000

    add steps in scenario

--- src/test/java/steps/GoogleSearchPage.java
+++ src/test/java/steps/GoogleSearchPage.java
@@ -10,10 +10,14 @@ public class GoogleSearchPage {
 
     private SelenideElement confirmCookieButton = $x("//div/button[@id='L2AGLb']");
     private SelenideElement clickSearchGoogle = $x("//div/textarea[@name='q']");
+    private SelenideElement linkCucumberTutorial = $x("(//div/cite[contains(text(), 'https://cucumber.io')])[1]");
 
-    private SelenideElement matchingTextInLinkCucumber = $x("/*[text()='Cucumber: BDD Testing & Collaboration Tools for Teams']");
+    private SelenideElement buttonAllowCookie = $x("(//div/button[contains(text(), 'Allow all cookies')])[1]");
 
-    private SelenideElement linkCucumber = $x("https://cucumber.io/");
+    private SelenideElement topicDocs = $x("//a[text()='Docs' and @href='#']");
+
+    private SelenideElement buttonInstallation = $x("//div/a[@href='/docs/installation/' and text()='Installation']");
+
     @Given("confirm cookie in start google page")
     public void confirm_cookie_in_start_google_page() {
         confirmCookieButton.click();
@@ -25,15 +29,31 @@ public class GoogleSearchPage {
         clickSearchGoogle.setValue(searchValue).pressEnter();
     }
 
-    @And("find an article on the official website of Cucumber")
-    public void found_off_website_of_cucumber() {
-     matchingTextInLinkCucumber.shouldHave(Condition.text("Cucumber: BDD Testing & Collaboration Tools for Teams"));
-     linkCucumber.click();
+    @And("and click on the official page Cucumber")
+    public void and_click_on_the_official_page_Cucumber() {
+        linkCucumberTutorial.shouldBe(Condition.visible, Condition.enabled).shouldHave(Condition.text("cucumber")).click();
 
     }
 
+    @And("allow cookie on official website")
+    public void allow_cookie_on_official_website() {
+        buttonAllowCookie.shouldHave(Condition.visible, Condition.enabled).click();
+    }
+
+    @And("click on the topic Docs in drop down menu")
+    public void click_on_the_topic_Docs_in_drop_down_menu() {
+        topicDocs.shouldBe(Condition.visible, Condition.enabled).click();
+    }
+
+    @And("click on the button Installation in drop down menu")
+    public void click_on_the_button_Installation_in_drop_down_menu() {
+        buttonInstallation.shouldBe(Condition.visible, Condition.enabled).shouldHave(Condition.text("Installation")).click();
+    }
+
+
     @Then("ensure that topic of quick start is onsite")
     public void ensure_that_topic_of_quick_start_is_onsite() {
+
 
     }
 
